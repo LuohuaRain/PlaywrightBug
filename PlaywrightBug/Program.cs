b8e6079eb6ec90155d03a47fd79b98d1f690433e
LuohuaRain <wangavatar95@gmail.com>
2021-08-28 19:55:13 +0800

    Add project files.

---
+++ PlaywrightBug/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Playwright;
+using System;
+
+using var playwright = await Playwright.CreateAsync();
+await using var browser = await playwright.Webkit.LaunchAsync();
+var i = 0;
+while (true)
+{
+    i++;
+    await using var context = await browser.NewContextAsync();
+    var page = await context.NewPageAsync();
+    var response = await page.GotoAsync("https://www.microsoft.com");
+    Console.WriteLine($"[{i}]response {response.Status}, browser context count:{browser.Contexts.Count}");
+}
