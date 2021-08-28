c2d0a2c30c2d091ecc2867f31d00b6cc4b73462f
LuohuaRain <wangavatar95@gmail.com>
2021-08-28 20:10:51 +0800

    add try catch

--- PlaywrightBug/Program.cs
+++ PlaywrightBug/Program.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.Playwright;
 using System;
+using System.Reflection.Metadata;
 
 using var playwright = await Playwright.CreateAsync();
 await using var browser = await playwright.Webkit.LaunchAsync();
 var i = 0;
 while (true)
 {
-    i++;
-    await using var context = await browser.NewContextAsync();
-    var page = await context.NewPageAsync();
-    var response = await page.GotoAsync("https://www.microsoft.com");
-    Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}][{i}]response {response.Status}, browser context count:{browser.Contexts.Count}");
+    try
+    {
+        i++;
+        await using var context = await browser.NewContextAsync();
+        var page = await context.NewPageAsync();
+        var response = await page.GotoAsync("https://www.microsoft.com");
+        Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}][{i}]response {response.Status}, browser context count:{browser.Contexts.Count}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
 }
